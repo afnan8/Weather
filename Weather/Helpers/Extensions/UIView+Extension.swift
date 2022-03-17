42f35add7f5a6d15b3e784f1c36f30e07aba75f2
afnan8 <afnan.h.mohamed@gmail.com>
2022-03-17 22:23:47 +0200

    custom method to add background view

--- Weather/Helpers/Extensions/UIView+Extension.swift
+++ Weather/Helpers/Extensions/UIView+Extension.swift
@@ -10,12 +10,33 @@ import UIKit
 
 extension UIView {
     
-    func gradientBackgroundColor() {
+    func createBackground() {
+        let view = gradientBackgroundColor()
+        let backgroundImageView: UIImageView = {
+            let imageView = UIImageView()
+            imageView.image = #imageLiteral(resourceName: "Background")
+            imageView.contentMode = .scaleAspectFill
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.layer.zPosition = 1
+            return imageView
+        }()
+        view.addSubview(backgroundImageView)
+        backgroundImageView.snp.makeConstraints() {
+            make in
+            make.height.equalTo(view.snp.height).multipliedBy(0.3)
+            make.bottom.equalTo(view.snp.bottom)
+            make.left.equalTo(view.snp.left)
+            make.right.equalTo(view.snp.right)
+        }
+    }
+    
+    private func gradientBackgroundColor() -> UIView {
         let view = GradientView()
         view.startColor = .topGradientColor()
         view.endColor = .bottomGradientColor()
         view.frame = self.frame
         self.addSubview(view)
+        return view
     }
     
     static var identifier: String {
--- Weather/Helpers/Extensions/UIViewController+Extension.swift
+++ Weather/Helpers/Extensions/UIViewController+Extension.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+protocol NavigationRoute {
+    func navigate(to route: Route)
+}
+
+extension UIViewController: NavigationRoute {
+    
+    func navigate(to route: Route) {
+        switch route.style {
+        case .modal:
+            self.present(route.destination, animated: true, completion: nil)
+        case .push:
+            self.navigationController?.pushViewController(route.destination, animated: true)
+        }
+    }
+}
