4619bf5af76cc4f9113f9840eb1a6a02a5cdeb82
afnan8 <afnan.h.mohamed@gmail.com>
2022-03-17 22:25:23 +0200

    create citySearch viewController

--- Weather/Scenes/CitySearch/CitySearchViewController.swift
+++ Weather/Scenes/CitySearch/CitySearchViewController.swift
@@ -9,21 +9,85 @@ import UIKit
 
 class CitySearchViewController: UIViewController {
 
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .textColor()
+        label.text = "Enter city, postcode or airoport location".uppercased()
+        label.font = UIFont.appFontRegular(ofSize: 13)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.isTranslucent = true
+        searchBar.placeholder = "Search"
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        return searchBar
+    }()
+    
+    let cancelButton: UIButton = {
+        let button = UIButton()
+        button.contentHorizontalAlignment = .center
+        button.titleLabel?.font = UIFont.appFontRegular(ofSize: 22)
+        button.setTitle("Cancel", for: .normal)
+        button.setTitleColor(.accentColor(), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(cancelSearch(_:)), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        view.createBackground()
     }
-    */
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        layout()
+    }
+    
+    func layout() {
+        view.addSubview(titleLabel)
+        view.addSubview(searchBar)
+        view.addSubview(cancelButton)
+        
+        titleLabel.snp.makeConstraints() {
+            make in
+            make.left.right.equalTo(view)
+            make.top.equalTo(additionalSafeAreaInsets.top).offset(16)
+        }
+     
+        searchBar.snp.makeConstraints() {
+            make in
+            make.left.equalTo(view.snp.left).offset(16)
+            make.width.equalTo(view.snp.width).multipliedBy(0.7)
+            make.height.equalTo(view.snp.height).multipliedBy(0.06)
+            make.top.equalTo(titleLabel.snp.bottom).offset(19)
+        }
+        
+        cancelButton.snp.makeConstraints() {
+            make in
+            make.left.equalTo(searchBar.snp.right)
+            make.right.equalTo(view.snp.right).offset(-16)
+            make.top.bottom.equalTo(searchBar)
+        }
+    }
+    
+    @objc func cancelSearch(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
+}
+extension CitySearchViewController: UISearchBarDelegate {
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        //create request and save to core data
+        self.dismiss(animated: true)
+    }
 }
